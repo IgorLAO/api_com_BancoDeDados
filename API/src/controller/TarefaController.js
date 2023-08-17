bccd0a004a0c8bf4f728678f5545efa0bf575bfa
igorLAO <igooli670@gmail.com>
2023-08-17 18:38:12 -0300

    api alt

--- API/src/controller/TarefaController.js
+++ API/src/controller/TarefaController.js
@@ -1,13 +1,36 @@
 import { Router } from "express";
-import {  listarTarefas } from "../repository/tarefaRepository.js";
+import  {  listarTarefas, InsertTask, DeleteTask } from "../repository/tarefaRepository.js";
 
 let endpoint = Router();
 
-endpoint.get('/lista', async (req, resp) =>{
+endpoint.get('/tasks', async (req, resp) =>{
     let dados = await listarTarefas();
     resp.send(dados);
 })
 
+
+endpoint.post('/task', async(req, resp) =>{
+        try {
+            const AddTask = req.body;
+
+            const data = await InsertTask(AddTask);
+
+            resp.send(data)
+        } catch (err) {
+            resp.send(400).send({
+                erro: err.message
+            })
+        }
+})
+
+endpoint.delete('/tasks/:id', async (req, resp) =>{
+    const { id } = req.params;
+
+    const response = await DeleteTask(id)
+
+    resp.status(204).send()
+
+})
 
 
 
--- API/src/repository/tarefaRepository.js
+++ API/src/repository/tarefaRepository.js
@@ -1,11 +1,26 @@
-import conexao from "./conection.js";
+import connectionDB from "./conection.js";
 
 export async function listarTarefas(){
     let sql = `select *
                     from TB_TASKS`
     
-    let resp = await conexao.query(sql);
+    let resp = await connectionDB.query(sql);
     let data = resp[0];
     return data;
 }
 
+export async function InsertTask(task) {
+    let comand = `INSERT INTO TB_TASKS (DS_TASK, NR_ORDER, BT_FINISHED, DT_CADASTRO)
+                                VALUES (?, ?, ?, ?)`;
+
+    const [resp] = await connectionDB.query( comand, [task.DS_TASK, task.NR_ORDER, task.BT_FINISHED, task.DT_CADASTRO ]);
+    return task;
+}
+
+export async function DeleteTask(id){
+    let sql = `DELETE FROM TB_TASKS
+                        WHERE ID_TASK = ?`
+
+    const [resp]  = await connectionDB.query( sql, [id])  
+    return id
+}
